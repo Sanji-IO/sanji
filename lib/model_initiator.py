0d003e13134e25b1c5cfa1c2e2da83ad96cf85f6
ChinFu.Yang <chinfu.yang@moxa.com>
2014-08-12 14:22:22 +0800

    Split file

---
+++ lib/model_initiator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+class ModelInitiator(object):
+    '''
+    '   Deal with some model initialization works like DB and Condifuration files creating.
+    '''
+    def __init__(self, model_name, model_path, db_type="json"):
+        self.model_name = model_name
+        self.model_path = model_path
+        self.db_type = db_type
+
+    def mkdir(self):
+        data_folder_path = self.model_path + "/data"
+
+        if not os.path.exists(data_folder_path):
+            os.mkdir(data_folder_path)
+
+    def create_db(self):
+        factory_json_db_path = self.model_path + "/data/" + self.model_name + ".factory.json"
+        json_db_path = self.model_path + "/data/" + self.model_name + ".json"
+
+        if self.db_type == "json":
+            if not os.path.exists(json_db_path):
+                if os.path.exists(factory_json_db_path):
+                    shutil.copy2(factory_json_db_path, json_db_path)
+
+
+    def __del__(self):
+        pass
--- lib/sanji.py
+++ lib/sanji.py
@@ -10,6 +10,8 @@ import sys
 import os
 import router
 
+
+
 '''
 Status Codes
 '''
@@ -78,66 +80,39 @@ class Sanji(object):
         self._mqtt.loop_stop()
         sys.exit(0)
 
-		#client = mqtt.Client()
-		client = mosquitto.Mosquitto(client_id="38129")
-		client.on_connect = self.on_connect
-		client.on_message = self.on_message
-		client.on_publish = self.on_publish
-		client.on_subscribe = self.on_subscribe
-		client.on_log = self.on_log
+        #client = mqtt.Client()
+        client = mosquitto.Mosquitto(client_id="38129")
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.on_publish = self.on_publish
+        client.on_subscribe = self.on_subscribe
+        client.on_log = self.on_log
 
 
-		print "start to connect"
-		client.connect("127.0.0.1", 1883, 60)
-		print "end connect"
+        print "start to connect"
+        client.connect("127.0.0.1", 1883, 60)
+        print "end connect"
 
-		# Blocking call that processes network traffic, dispatches callbacks and
-		# handles reconnecting.
-		# Other loop*() functions are available that give a threaded interface and a
-		# manual interface.
-		client.loop_forever()
-
-
-
-	def on_connect(mqttc, obj, flags, rc):
-		print("rc: "+str(rc))
-	def on_message(mqttc, obj, msg):
-		print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-	def on_publish(mqttc, obj, mid):
-		print("mid: "+str(mid))
-	def on_subscribe(mqttc, obj, mid, granted_qos):
-		print("Subscribed: "+str(mid)+" "+str(granted_qos))
-	def on_log(mqttc, obj, level, string):
-		print(string)
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        # Other loop*() functions are available that give a threaded interface and a
+        # manual interface.
+        client.loop_forever()
 
 
-class ModelInitiator(object):
-    '''
-    '   Deal with some model initialization works like DB and Condifuration files creating.
-    '''
-    def __init__(self, model_name, model_path, db_type="json"):
-        self.model_name = model_name
-        self.model_path = model_path
-        self.db_type = db_type
 
-    def mkdir(self):
-        data_folder_path = self.model_path + "/data"
-
-        if not os.path.exists(data_folder_path):
-            os.mkdir(data_folder_path)
-
-    def create_db(self):
-        factory_json_db_path = self.model_path + "/data/" + self.model_name + ".factory.json"
-        json_db_path = self.model_path + "/data/" + self.model_name + ".json"
-
-        if self.db_type == "json":
-            if not os.path.exists(json_db_path):
-                if os.path.exists(factory_json_db_path):
-                    shutil.copy2(factory_json_db_path, json_db_path)
+    def on_connect(mqttc, obj, flags, rc):
+        print("rc: "+str(rc))
+    def on_message(mqttc, obj, msg):
+        print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+    def on_publish(mqttc, obj, mid):
+        print("mid: "+str(mid))
+    def on_subscribe(mqttc, obj, mid, granted_qos):
+        print("Subscribed: "+str(mid)+" "+str(granted_qos))
+    def on_log(mqttc, obj, level, string):
+        print(string)
 
 
-    def __del__(self):
-        pass
 
 
 
