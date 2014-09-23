b391a384ae67bcbf445766313d1b50cf24b3049e
Zack YL Shih <zackyl.shih@moxa.com>
2014-09-23 09:57:55 +0800

    Add setup.py for python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sanji',
+      version='0.1',
+      description='Sanji SDK',
+      url='https://github.com/Sanji-IO',
+      author='ZackYL Shih',
+      author_email='zackyl.shih@moxa.com',
+      license='MOXA',
+      packages=['sanji'],
+      zip_safe=False)
