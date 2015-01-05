6531b28b9eab7fdafcdd0c3892c5a07ab3f1b7c2
Zack YL Shih <zackyl.shih@moxa.com>
2015-01-05 16:13:58 +0800

    Prepare for pypi

---
+++ README.rst
@@ -0,0 +1,28 @@
+Sanji Framework SDK (Python)
+============================
+.. image:: https://travis-ci.org/Sanji-IO/sanji.svg
+    :target: https://travis-ci.org/Sanji-IO/sanji
+    :alt: Build Status
+
+.. image:: https://pypip.in/version/modem-cmd/badge.svg
+    :target: https://pypi.python.org/pypi/modem-cmd/
+    :alt: Latest Version
+
+.. image:: https://coveralls.io/repos/Sanji-IO/sanji/badge.png?branch=develop
+    :target: https://coveralls.io/r/Sanji-IO/sanji?branch=develop
+    :alt: Coverage Status
+
+Installation
+------------
+::
+
+    pip install sanji
+
+Quick Start
+-----------
+**Comming soon...**
+
+License
+-------
+
+MIT
--- setup.py
+++ setup.py
@@ -1,13 +1,33 @@
 from setuptools import setup
-import sanji
+import os
 
-setup(name="sanji",
-      version=sanji.__version__,
-      description="Sanji SDK",
-      url="https://github.com/Sanji-IO",
-      author="Sanji Team",
-      author_email="sanji@moxa.com",
-      license="MIT",
-      packages=["sanji", "sanji.connection"],
-      install_requires=["voluptuous"]
-      )
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+
+setup(
+    name="sanji",
+    version="0.0.1",
+    description="Sanji Framework SDK",
+    long_description=read('README.rst'),
+    url="https://github.com/Sanji-IO/sanji",
+    author="Sanji Team",
+    author_email="sanji@moxa.com",
+    license="MIT",
+    packages=["sanji", "sanji.connection"],
+    install_requires=["voluptuous"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
